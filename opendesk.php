94965ec6e15e46f8c40efdad94669a0e1d2cd747
karmela <karmela036@gmail.com>
2018-05-02 08:51:13 +0200

    first commit

---
+++ opendesk.php
@@ -0,0 +1,229 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+  </head>
+
+  <body>
+    <header>
+        <div class="row">
+            <div class="col-md-8"> 
+                <nav class="navbar navbar-expand-lg">
+                    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                        <div class="navbar-nav">
+                            <a class="nav-item nav-link active" href="#"><img src="logo.jpg" alt="Opendesk" width="35%"><span class="sr-only">(current)</span></a>
+                            <a class="nav-item nav-link" href="#"> Furniture </a>
+                            <a class="nav-item nav-link" href="#"> Workspaces </a>
+                            <a class="nav-item nav-link" href="#"> Open Making </a>
+                            <a class="nav-item nav-link" href="#"> Blog </a>
+                        </div>
+                    </div>
+                </nav>
+            </div> 
+
+         <div class="col-md-4">
+            <nav class="navbar navbar-expand-lg">
+                    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                        <div class="navbar-nav">
+                            <a class="nav-item nav-link active" href="#"><i class="fas fa-shopping-cart"></i><span class="sr-only">(current)</span></a>
+                            <a class="nav-item nav-link" href="#"> Your dashboard  </a>
+                            <a class="nav-item nav-link" href="#"> Join as a maker </a>                           
+                        </div>
+                    </div>
+            </nav>
+        </div>
+    </header> 
+  
+
+    <div class="card bg-dark text-white" id="imagen">
+        <img id="imagen" class="card-img" src="imagen1.jpg" alt="Card image">
+        <div class="card-img-overlay">
+            <h2 class="card-title"> Furniture designed for inspiring workplaces </h2>
+            <p class="card-text"> We’re changing the way furniture is made, by connecting customers to local makers </p>
+            <button type="button" class="btn btn-success"> Shop Furniture </button>
+        </div>
+    </div>
+
+    <div class="card-group">
+        <div class="card">
+            <img class="card-img-top" src="card1.jpg" alt="Card image cap">
+            <div class="card-body">
+                <h5 class="card-title"> Lean Desk </h5>
+                <p class="card-text"><small class="text-muted"> Generous four seat workstation </small></p>
+            </div>
+        </div>
+        <div class="card">
+            <img class="card-img-top" src="card2.jpg" alt="Card image cap">
+            <div class="card-body">
+            <h5 class="card-title">Linnea Bookshelf </h5>
+            <p class="card-text"><small class="text-muted"> Elegant shelving & storage solution </small></p>            
+        </div>
+        </div>
+        <div class="card">
+            <img class="card-img-top" src="card3.jpg" alt="Card image cap">
+            <div class="card-body">
+            <h5 class="card-title"> Johann Bench </h5>
+            <p class="card-text"><small class="text-muted">2 person canteen and meeting bench </small></p>
+        </div>
+        </div>
+    </div>
+    
+    <div class="card bg-dark text-white" id="imagen">
+        <img class="card-img" src="imagen2.jpg" alt="Card image">
+        <div class="card-img-overlay">
+            <h2 class="card-title"> A global platform for local making </h2>
+            <p class="card-text"> Opendesk is an online marketplace that hosts independently designed furniture and connects its customers to local makers around the world. Rather than mass manufacturing and shipping worldwide, we’re building a distributed and ethical supply chain through a global maker network. </p>
+        </div>
+    </div>
+       
+    <center><h2 class="h5"> HOW IT WORKS </h2></center>
+
+    <div class="card-group">
+        <div class="card">
+            <img class="card-img-top" src="" alt="Card image cap">
+            <div class="card-body">
+                <h2 class="card-title"> Select designs </h2>
+                <p class="card-text"><small class="text-muted">Browse and add designs to your basket or get in touch for help </small></p>
+            </div>
+        </div>
+        <div class="card">
+            <img class="card-img-top" src="" alt="Card image cap">
+            <div class="card-body">
+                <h2 class="card-title"> Request quotes </h2>
+                <p class="card-text"><small class="text-muted">Get quotes from local makers in your area within two business days </small></p>          
+            </div>
+        </div>
+        <div class="card">
+            <img class="card-img-top" src="" alt="Card image cap">
+            <div class="card-body">
+                <h2 class="card-title"> Choose your maker </h2>
+                <p class="card-text"><small class="text-muted">Choose the local maker's quote that works best for you </small></p>
+            </div>
+        </div>
+        <div class="card">
+            <img class="card-img-top" src="" alt="Card image cap">
+            <div class="card-body">
+                <h2 class="card-title"> Receive your furniture </h2>
+                <p class="card-text"><small class="text-muted"> Get furniture delivered to your door in 2-4 weeks, direct from your maker </small></p>
+            </div>
+        </div>
+    </div>
+
+    <div>
+            <button type="button" class="btn btn-success"> Get started </button>
+    </div>
+
+    <div>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/efh8wb-1uSA" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>    
+    </div>
+
+    <div class="logo">
+        <img src="logo1.jpg" alt="..." width="...">
+        <img src="logo2.jpg" alt="..." width="...">
+        <img src="logo3.jpg" alt="..." width="...">
+        <img src="logo4.jpg" alt="..." width="...">
+    </div>
+    <div>
+        <div class="row">
+            <img src="nice.jpg" alt="" width="20px"> <img src="wework.jpg" alt="" width="20px"> <img src="google.jpg" alt="" width="20px"><img src="greenpeace.jpg" alt="" width="20px">   
+        </div>
+    </div>
+ 
+    <div class="card bg-dark text-white" id="imagen">
+        <img class="card-img" src="imagen3.jpg" alt="Card image">
+        <div class="card-img-overlay">
+            <h2 class="card-title"> Inspiring workplaces for amazing companies </h2>
+            <p class="card-text"> We spend one third of our lives working, so our work environments matter. These companies bought Opendesk designs from local makers because they’re beautiful, made on-demand and built for collaboration </p>
+            <button type="button" class="btn btn-light"> View case studies </button>
+        </div>
+    </div>
+
+    <div>
+        <h2>The Guardian</h2>
+        <p> “Opendesk is changing the way furniture is produced and moved, cutting out time-consuming and expensive shipping, showrooms and storage.” </P>
+    </div>
+
+    <div class="img">
+        <img src="img1.jpg" alt="..." max-width="100%">
+        <img src="img2.jpg" alt="..." width="...">
+        <img src="img3.jpg" alt="..." width="...">
+        <img src="img4.jpg" alt="..." width="...">
+        <img src="img5.jpg" alt="..." width="...">
+    </div>
+
+<footer>
+    <div class="row">
+        <p><strong> LOGO OPENDESK </strong></p>
+    </div>
+    <div class="row">
+        <ul>
+            <li> About </li>
+            <li> Contact </li>
+            <li> FAQ </li>
+            <li> Our Terms </li>
+            <li> Jobs </li>
+        </ul>
+    </div>
+    <div class="row">   
+        <p>
+            <i class="fab fa-twitter"></i>
+            <i class="fab fa-twitter"></i>
+            <i class="fab fa-facebook"></i>
+            <i class="fab fa-instagram"></i>
+            <i class="fab fa-pinterest-square"></i>
+            <i class="fab fa-linkedin"></i>
+            <i class="fab fa-vimeo-square"></i>
+            <i class="fab fa-youtube"></i>
+        </p>  
+    </div>           
+      
+    <div>
+        <hr> </hr>
+    </div>
+
+    <div class="row">
+        <div class="col-md-6">
+            <button type="button" class="btn btn-light"> Sign up to our newsletter </button>
+        </div>
+        <div class="col-md-6">
+            <p> Let us know your thoughts about things you love and things that can be improved on the site: </p>
+            <a href="https://www.opendesk.cc/the-art-of-sharing"> https://www.opendesk.cc/the-art-of-sharing </a>
+        </div>
+    </div>
+
+    <div class="row">
+        <hr> </hr>
+    </div>
+
+    <div class="row">
+        <div class="col-md-7">
+            <p> Opendesk uses cookies. By continuing to browse the site you’re agreeing to this. </p>
+        </div>
+        <div class="col-md-5">
+            <button type="button" class="btn btn-dark"> Ok, Thanks </button>
+        </div>
+    </div>
+
+    </footer>
+
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <script src="app.js"></script>
+</body>
+</html>
---
+++ opendesk_jesus.php
@@ -0,0 +1,238 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="style_jesus.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+  </head>
+
+  <body>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a href=""><img src="logo.jpg" alt="Opendesk" height="80"></a>
+                <div class="collapse navbar-collapse" id="navbarText">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="#"> Furniture <span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#"> Workspaces </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#"> Open Making </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#"> Blog </a>
+                        </li>
+                    </ul>
+                    <span class="navbar-text">
+                        <ul class="navbar-nav mr-auto">
+                            <li class="nav-item active">
+                                <a class="nav-link" href="#"> <i class="fas fa-shopping-cart"></i> <span class="sr-only">(current)</span></a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#"> Your dashboard </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#"> Join as a maker </a>
+                            </li>
+                        </ul>
+                    </span>
+                </div>
+        </nav>
+    </header> 
+  
+    <div id="uno" class="filaimagen">
+        <div>
+            <h2> Furniture designed for <br> inspiring workplaces </h2>
+            <p></p>
+            <h4> We’re changing the way furniture is <br> made, by connecting customers to local <br> makers </h4>
+            <p></p>
+            <button class="btn btn-success"> Shop Furniture </button>
+        </div>
+    </div>
+
+    <div id="muebles" >
+        <div class="card-group">
+            <div class="card">
+                <img class="card-img-top" src="card1.jpg" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title"> Lean Desk </h5>
+                    <p class="card-text"><small class="text-muted"> Generous four seat workstation </small></p>
+                </div>
+            </div>
+            <div class="card">
+                <img class="card-img-top" src="card2.jpg" alt="Card image cap">
+                <div class="card-body">
+                <h5 class="card-title"> Linnea Bookshelf </h5>
+                <p class="card-text"><small class="text-muted"> Elegant shelving & storage solution </small></p>            
+            </div>
+            </div>
+            <div class="card">
+                <img class="card-img-top" src="card3.jpg" alt="Card image cap">
+                <div class="card-body">
+                <h5 class="card-title"> Johann Bench </h5>
+                <p class="card-text"><small class="text-muted">2 person canteen and meeting bench </small></p>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <div id="dos" class="filaimagen">
+        <div>
+            <h2> A global platform for local <br> making </h2>
+            <h4> Opendesk is an online marketplace that <br>hosts independently designed furniture <br>and connects its customers to local <br>makers around the world. Rather than <br>mass manufacturing and shipping <br>worldwide, we’re building a distributed <br>and ethical supply chain through a global <br>maker network. </h4>
+        </div>
+    </div>
+  
+    <div id="pasos" class=" text-center">
+        <p> HOW IT WORKS </p> 
+        <div class="row ">
+            <div class="col">
+                <h1><i class="fab fa-angular"></i></h1>
+                <h3> Select design </h3>
+                <p> Browse and add designs to your basket or get in touch for help </p>
+            </div>
+            <div class="col">
+                <h1><i class="fab fa-node"></i></h1>
+                <h3> Request quotes</h3>
+                <p> Get quotes from local makers in your area within two business days </p>
+            </div>
+            <div class="col">
+            <h1><i class="fab fa-laravel"></i></h1>
+                <h3> Choose your maker </h3>
+                <p> Choose the local maker's quote that works best for you </p>
+            </div>
+            <div class="col">
+            <h1><i class="fab fa-react"></i></h1>
+                <h3> Receive your furniture </h3>
+                <p> Get furniture delivered to your door in 2-4 weeks, direct from your maker </p>
+            </div>
+        </div>        
+            <button type="button" class="btn btn-success"> Get started </button>
+    </div>
+   
+    <div id="mivideo">
+        <div style="padding:56.25% 0 0 0;position:relative;">
+            <iframe src="https://player.vimeo.com/video/215745438" 
+                style="position:absolute;top:0;left:0;width:100%;height:100%;" 
+                frameborder="0" 
+                webkitallowfullscreen 
+                mozallowfullscreen 
+                allowfullscreen>
+            </iframe>
+        </div>
+        <!-- <script src="https://player.vimeo.com/api/player.js"></script> -->
+    </div>
+
+  
+
+
+    <div class="logo">
+        <img src="logo1.jpg" alt="..." width="...">
+        <img src="logo2.jpg" alt="..." width="...">
+        <img src="logo3.jpg" alt="..." width="...">
+        <img src="logo4.jpg" alt="..." width="...">
+    </div>
+    <div>
+        <div class="row">
+            <img src="nice.jpg" alt="" width="20px"> <img src="wework.jpg" alt="" width="20px"> <img src="google.jpg" alt="" width="20px"><img src="greenpeace.jpg" alt="" width="20px">   
+        </div>
+    </div>
+ 
+    <div class="card bg-dark text-white" id="imagen">
+        <img class="card-img" src="imagen3.jpg" alt="Card image">
+        <div class="card-img-overlay">
+            <h2 class="card-title"> Inspiring workplaces for amazing companies </h2>
+            <p class="card-text"> We spend one third of our lives working, so our work environments matter. These companies bought Opendesk designs from local makers because they’re beautiful, made on-demand and built for collaboration </p>
+            <button type="button" class="btn btn-light"> View case studies </button>
+        </div>
+    </div>
+
+    <div>
+        <h2>The Guardian</h2>
+        <p> “Opendesk is changing the way furniture is produced and moved, cutting out time-consuming and expensive shipping, showrooms and storage.” </P>
+    </div>
+
+    <div class="img">
+        <img src="img1.jpg" alt="..." max-width="100%">
+        <img src="img2.jpg" alt="..." width="...">
+        <img src="img3.jpg" alt="..." width="...">
+        <img src="img4.jpg" alt="..." width="...">
+        <img src="img5.jpg" alt="..." width="...">
+    </div>
+
+<footer>
+    <div class="row">
+        <p><strong> LOGO OPENDESK </strong></p>
+    </div>
+    <div class="row">
+        <ul>
+            <li> About </li>
+            <li> Contact </li>
+            <li> FAQ </li>
+            <li> Our Terms </li>
+            <li> Jobs </li>
+        </ul>
+    </div>
+    <div class="row">   
+        <p>
+            <i class="fab fa-twitter"></i>
+            <i class="fab fa-twitter"></i>
+            <i class="fab fa-facebook"></i>
+            <i class="fab fa-instagram"></i>
+            <i class="fab fa-pinterest-square"></i>
+            <i class="fab fa-linkedin"></i>
+            <i class="fab fa-vimeo-square"></i>
+            <i class="fab fa-youtube"></i>
+        </p>  
+    </div>           
+      
+    <div>
+        <hr> </hr>
+    </div>
+
+    <div class="row">
+        <div class="col-md-6">
+            <button type="button" class="btn btn-light"> Sign up to our newsletter </button>
+        </div>
+        <div class="col-md-6">
+            <p> Let us know your thoughts about things you love and things that can be improved on the site: </p>
+            <a href="https://www.opendesk.cc/the-art-of-sharing"> https://www.opendesk.cc/the-art-of-sharing </a>
+        </div>
+    </div>
+
+    <div class="row">
+        <hr> </hr>
+    </div>
+
+    <div class="row">
+        <div class="col-md-7">
+            <p> Opendesk uses cookies. By continuing to browse the site you’re agreeing to this. </p>
+        </div>
+        <div class="col-md-5">
+            <button type="button" class="btn btn-dark"> Ok, Thanks </button>
+        </div>
+    </div>
+
+    </footer>
+
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <script src="app.js"></script>
+</body>
+</html>
